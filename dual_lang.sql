fc8bc76469e73f85595313c38b04f48fe016ccb0
Matt Barton <100886744+mattbarton08@users.noreply.github.com>
2023-07-06 10:51:16 -0400

    Add files via upload
    SQL file for dual_lang table

---
+++ dual_lang.sql
@@ -0,0 +1,16 @@
+CREATE TABLE dual_lang (
+	Index_ INT,
+	Education_District VARCHAR,
+	District_Name Varchar,
+	School_Name VARCHAR,
+	County VARCHAR,
+	School_Level VARCHAR,
+	Language_ VARCHAR,
+	Type_of_Immersion VARCHAR,
+	Type_of_Program VARCHAR,
+	Structure_ VARCHAR,
+	Year_of_Launch VARCHAR,
+	Additional_Initiatives VARCHAR
+	);
+	
+	SELECT * FROM dual_lang;
